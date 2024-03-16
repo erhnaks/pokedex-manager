8f804aa4985dd166c7bf464e4cf1cfdbf851058e
erhnaks <erhnaks@hotmail.com>
2024-03-16 10:02:04 +0000

    initial commit

---
+++ src/main/java/com/ozan/pokedexmanager/PokedexManagerApplication.java
@@ -0,0 +1,13 @@
+package com.ozan.pokedexmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokedexManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokedexManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ozan/pokedexmanager/PokedexManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ozan.pokedexmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedexManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
